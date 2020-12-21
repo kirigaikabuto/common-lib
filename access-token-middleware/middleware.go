1cf94927ef6c9cf071d9ccc007c645d139ad6fc0
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-21 17:27:12 +0600

    sdsdsds

---
+++ access-token-middleware/middleware.go
@@ -0,0 +1,50 @@
+package access_token_middleware
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type AccessTokenMDW struct {
+	accessTokenStore AccessTokenStore
+}
+
+type customError struct {
+	Message string `json:"message"`
+}
+
+func NewAccessTokenMDW(accessTokenStore AccessTokenStore) (*AccessTokenMDW, error) {
+	return &AccessTokenMDW{accessTokenStore}, nil
+}
+
+func (atmdw *AccessTokenMDW) Middleware(fn http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := r.Header.Get("Authorization")
+		if key == "" {
+			respondJSON(w, http.StatusInternalServerError, &customError{"For access needed authorization header"})
+			return
+		}
+		if key != "" {
+			userId, err := atmdw.accessTokenStore.Get(key)
+			if err != nil {
+				respondJSON(w, http.StatusInternalServerError, &customError{"Not correct access key"})
+				return
+			}
+			ctx := context.WithValue(r.Context(), "user_id", userId)
+			r = r.WithContext(ctx)
+		}
+	})
+}
+
+func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write([]byte(response))
+}
